b14b63d262aab970145a066061335d12bbb5d5bc
Jonathan Expósito <alu0100696455@ull.edu.es>
2016-03-01 15:43:05 +0000

    Implementada la herencia :shipit:

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,59 +1,76 @@
 (function(exports) {
   "use strict";
 
-  function Medida(valor,tipo)  
-  {
-    /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
-    /* ademas de new Medida(45.2, "Km") */
+  function Medida(valor, tipo) {
+    var value = valor;
+    var type = tipo || "indefinido";
+
+    this.getValue = function() {
+      return value;
+    };
   }
-  
-  function Temperatura(valor,tipo)
-  {
-    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+
+  function Temperatura(valor, tipo) {
+    Medida.call(this, valor, tipo);
+
+    this.toCelsius = function() {
+      return this.getValue();
+    }
+    this.toFahrenheit = function() {
+      return this.getValue();
+    }
   }
-  
-  function Celsius(valor)
-  {
+
+  Temperatura.prototype = new Medida();
+  Temperatura.prototype.constructor = Temperatura;
+
+  function Celsius(valor) {
+    Temperatura.call(this, valor, "Celsius");
   }
-  
-  function Farenheit(valor)
-  {
+
+  Celsius.prototype = new Temperatura();
+  Celsius.prototype.constructor = Celsius;
+
+  function Fahrenheit(valor) {
+    Temperatura.call(this, valor, "Fahrenheit");
   }
-  
+
+  Fahrenheit.prototype = new Temperatura();
+  Fahrenheit.prototype.constructor = Fahrenheit;
+
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
-  exports.Farenheit = Farenheit;
+  exports.Fahrenheit = Fahrenheit;
 
   exports.convertir = function() {
-    var valor     = document.getElementById('convert').value,
-        elemento  = document.getElementById('converted'),
-        /* Extienda la RegeExp a la especificación. use una XRegExp */
-        regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i,
-        valor     = valor.match(regexp);
-    
+    var valor = document.getElementById('convert').value,
+      elemento = document.getElementById('converted'),
+      /* Extienda la RegeExp a la especificación. use una XRegExp */
+      regexp = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i,
+      valor = valor.match(regexp);
+
     if (valor) {
       var numero = valor[1],
-          tipo   = valor[2].toLowerCase();
-      
+        tipo = valor[2].toLowerCase();
+
       numero = parseFloat(numero);
       console.log("Valor: " + numero + ", Tipo: " + tipo);
-      
+
       switch (tipo) {
         case 'c':
           var celsius = new Celsius(numero);
-          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
+          elemento.innerHTML = celsius.toFahrenheit().toFixed(2) + " Fahrenheit";
           break;
         case 'f':
-          var farenheit = new Farenheit(numero);
-          elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
+          var fahrenheit = new Fahrenheit(numero);
+          elemento.innerHTML = fahrenheit.toCelsius().toFixed(2) + " Celsius";
           break;
-        
+
         default:
           /* rellene este código */
       }
-    }
-    else
+    } else
       elemento.innerHTML = "";
   }
-  
+
 })(this);
